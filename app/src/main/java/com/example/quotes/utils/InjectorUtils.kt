1dbff79e9494eb8796932f300961327682850999
Elijah <elijate@gmail.com>
2020-08-09 09:11:54 +0300

    Creates an injector util

---
+++ app/src/main/java/com/example/quotes/utils/InjectorUtils.kt
@@ -0,0 +1,15 @@
+package com.example.quotes.utils
+
+import com.example.quotes.model.Database
+import com.example.quotes.model.QuoteRepository
+import com.example.quotes.view.QuotesViewModelFactory
+
+class InjectorUtils {
+    // This will be called from QuotesActivity
+    fun provideQuotesViewModelFactory(): QuotesViewModelFactory {
+        // ViewModelFactory needs a repository, which in turn needs a DAO from a database
+        // The whole dependency tree is constructed right here, in one place
+        val quoteRepository = QuoteRepository.getInstance(Database.getInstance().quoteDao)
+        return QuotesViewModelFactory(quoteRepository)
+    }
+}
