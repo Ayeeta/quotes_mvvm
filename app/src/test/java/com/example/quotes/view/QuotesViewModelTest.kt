7fb411964e57b7df31b4a93fe77f5222d50e45ce
Elijah <elijate@gmail.com>
2020-08-14 22:55:36 +0300

    set up tests

---
+++ app/src/test/java/com/example/quotes/view/QuotesViewModelTest.kt
@@ -0,0 +1,26 @@
+package com.example.quotes.view
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class QuotesViewModelTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun getQuotes() {
+    }
+
+    @Test
+    fun addQuote() {
+    }
+}
